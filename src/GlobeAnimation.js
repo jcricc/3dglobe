c951e8c230a09d8f4d8ff2ec978829099c5d4355
jcricc <jritchie2001@gmail.com>
2024-03-20 19:14:37 -0400

    v1.0

--- src/GlobeAnimation.js
+++ src/GlobeAnimation.js
@@ -1,17 +1,22 @@
 import React, { useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Stars, OrbitControls } from '@react-three/drei';
-import { TextureLoader, MeshBasicMaterial } from 'three';
+import { TextureLoader, MeshBasicMaterial, SphereGeometry, MeshStandardMaterial } from 'three';
 import PropTypes from 'prop-types';
 
 const Globe = () => {
   const mesh = useRef(null);
 
+  // Create a texture loader to load the globe texture
+  const textureLoader = new TextureLoader();
+  const globeTexture = textureLoader.load('/path/to/globe.jpg');
+
   return (
     <mesh ref={mesh}>
-      <sphereGeometry ref={[5, 32, 32]} />
-      <meshStandardMaterial color="royalblue" />
-      {}
+      {/* Use SphereGeometry for the geometry */}
+      <sphereGeometry args={[5, 32, 32]} />
+      {/* Use MeshStandardMaterial for the material, with the globe texture as the map */}
+      <meshStandardMaterial map={globeTexture} />
     </mesh>
   );
 };
@@ -21,9 +26,8 @@ const FloatingIcon = ({ position, icon }) => {
   const material = new MeshBasicMaterial({ map: texture });
 
   return (
-    <mesh ref={position} material={material}>
+    <mesh position={position} material={material}>
       <planeGeometry args={[1, 1]} />
-      <meshBasicMaterial color="white" transparent opacity={0.8} />
       <sprite position={[0, 0, 0]} scale={[1, 1, 1]}>
         <spriteMaterial attach="material" map={texture} />
       </sprite>
